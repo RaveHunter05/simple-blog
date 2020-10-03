5530a102fcd7e70de47731d0fec87e124d9da1e8
RaveHunter05 <paulsotelo97@gmail.com>
2020-10-03 17:05:51 -0600

    agregando imagen a agregar blog

--- blog/controllers.js
+++ blog/controllers.js
@@ -12,26 +12,44 @@ let blogController = {
     },
     addBlog(req,res){
         let {title, content} =  req.body
-    
-        Blog.create({title,content})
-        .then(response => res.json({"respuesta": response}))
-        .catch(err => console.error("There was an error", err))
-    },
-    addImage(req,res){
-
-        let {image} = req.body
-
+        console.log(content)
         if(req.files){
-            let file = req.files.image
-            let fileName = file.name
-            file.mv(__dirname+ '/uploads/mainImages/' + fileName, function(err){
+                
+            let {image} = req.files
+            let fileName = image.name
+            let direction = __dirname + '/uploads/mainImages/' + fileName
+            image.mv(direction , function(err){
                 if(err){
                     res.send(err)
                 }else{
-                    res.send("File Uploaded")
+                    Blog.create({title, content, mainImage: direction })
+                    .then(response => res.json({"respuesta": response}))
+                    .catch(err => console.error("There was an error", err))
                 }
             })
+        }else{
+            Blog.create({title,content})
+            .then(response => res.json({"respuesta": response}))
+            .catch(err => console.error("There was an error", err))
         }
+        
+        
+    },
+    addImage(req,res){
+
+        let {image} = req.files
+        console.log(image)
+        // if(req.files){
+        //     let file = req.files.image
+        //     let fileName = file.name
+        //     file.mv(__dirname+ '/uploads/mainImages/' + fileName, function(err){
+        //         if(err){
+        //             res.send(err)
+        //         }else{
+        //             res.send("File Uploaded")
+        //         }
+        //     })
+        // }
     }
 }
 
