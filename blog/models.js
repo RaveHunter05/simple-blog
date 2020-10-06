c97bad9dde21b253c607c43c6ed2ccf6f5e21e44
RaveHunter05 <paulsotelo97@gmail.com>
2020-10-05 23:02:07 -0600

    agregando usuarios, categorias y llaves foraneas

--- blog/models.js
+++ blog/models.js
@@ -18,13 +18,57 @@ const Blog = db.define('Blog',{
     }
 })
 
-//Categorias
-
-const Categories = db.define('Categories', {
-    name: {
+const User = db.define('User', {
+    name:{
+        type:DataTypes.STRING,
+        allowNull: false
+    },
+    password:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    nickname:{
         type: DataTypes.STRING,
         allowNull: false
     }
 })
 
-module.exports = {Blog, Categories}
+const Category = db.define('Category', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+})
+
+// Associations
+
+// Blog - Categoria
+
+Category.hasMany(Blog,{
+    foreignKey: 'categories_id'
+},{
+    onDelete: 'cascade'
+})
+
+Blog.belongsTo(Category,{
+    foreignKey: 'categories_id'
+},{
+    onDelete: 'cascade'
+})
+
+// Blog - Usuario
+
+User.hasMany(Blog,{
+    foreignKey: 'users_id'
+},{
+    onDelete: 'cascade'
+})
+
+Blog.belongsTo(User,{
+    foreignKey: 'users_id'
+},{
+    onDelete: 'cascade'
+})
+
+module.exports = {Blog, Category, User}
