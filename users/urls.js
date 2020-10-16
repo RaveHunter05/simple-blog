c08f935cd8fea8f9c12b7623f22536e29a183d78
RaveHunter05 <paulsotelo97@gmail.com>
2020-10-15 23:41:37 -0600

    funciona, no se como pero funciona xD, descifrarlo queda para otro dia

--- users/urls.js
+++ users/urls.js
@@ -3,6 +3,8 @@ const router = app.Router();
 
 const jwt = require('jsonwebtoken')
 const passport = require('passport')
+const session = require('express-session')
+const PassportLocal = require('passport-local')
 
 const {User} = require('./models')
 
@@ -21,6 +23,30 @@ const {User} = require('./models')
 //     })
 // })
 
+passport.use(new PassportLocal(function(username, password, done){
+    if(username === "codigofacilito" && password === "123"){
+        return done(null, {id: 1, name: "Paul"})
+    }
+    done(null, false)
+}))
+
+passport.serializeUser(function(user, done){
+    done(null, user.id)
+})
+
+passport.deserializeUser(function(id, done){
+    done(null, {id: 1, name: "Paul" })
+})
+
+router.use(session({
+    secret: 'lorem ipsum dolor sit amet',
+    resave: true,
+    saveUninitialized: true
+}))
+
+router.use(passport.initialize())
+router.use(passport.session())
+
 router.post('/register', (req,res) =>{
     const {name, password, email, nickname} = req.body
     User.findOne({where:{email}})
@@ -29,12 +55,26 @@ router.post('/register', (req,res) =>{
             res.send('user already exists')
         }else{
             User.create({name, password, email, nickname})
+            // Aquí hace falta 
             .then(response => res.json({"respuesta": response}))
             .catch(err => console.error("There was an error", err))
         }
     })
     .catch(err => console.error(err))
     
+})
+
+router.post('/login', passport.authenticate('local', {
+    successRedirect: "/",
+    failureRedirect: "/error"
+}))
+
+router.get('/', (req,res) =>{
+    res.send('redirigido correctamente')
+})
+
+router.get('/error', (req,res) =>{
+    res.send('redirigido incorrectamente')
 })
 
 router.post('/logout', (req,res) =>{
